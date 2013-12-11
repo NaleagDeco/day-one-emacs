7bf1fb76a25b1639480add50890d84abdae76b32
Gaelan D'costa <gdcosta@gmail.com>
2013-12-11 00:43:27 -0500

    Committing single-file project skeleton.

---
+++ day-one.el
@@ -0,0 +1,17 @@
+;;; day-one.el --- Day One client for Emacs
+
+;; Copyright (C) 2013 Gaelan D'costa
+
+;; Author: Gaelan D'costa <gdcosta@gmail.com>
+;; Version: 0.1
+;; Keywords: dayone journal diary
+
+;;; Commentary:
+
+;; This package provides an interface for writing Day One journal entries in
+;; Emacs.
+
+;;; Code:
+
+(provide 'day-one)
+;;; day-one.el ends here
